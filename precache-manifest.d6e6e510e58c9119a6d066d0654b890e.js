c0bf7b46cac81309ba2dd16db1c7022d6f58f1e4
Vaibhavi-Rajesh <vaibhavi.rajesh22@gmail.com>
2022-07-04 11:37:08 +0800

    Updates

---
+++ precache-manifest.d6e6e510e58c9119a6d066d0654b890e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b199089e9c8d478b30d",
+    "url": "/react-portfolio-1/static/css/main.62d50fd8.chunk.css"
+  },
+  {
+    "revision": "2b199089e9c8d478b30d",
+    "url": "/react-portfolio-1/static/js/main.2b199089.chunk.js"
+  },
+  {
+    "revision": "70fb8bd8fc979d1df0d4",
+    "url": "/react-portfolio-1/static/css/1.c3e8447b.chunk.css"
+  },
+  {
+    "revision": "70fb8bd8fc979d1df0d4",
+    "url": "/react-portfolio-1/static/js/1.70fb8bd8.chunk.js"
+  },
+  {
+    "revision": "d763e89468735bdf5c00",
+    "url": "/react-portfolio-1/static/js/runtime~main.d763e894.js"
+  },
+  {
+    "revision": "cc26a3e11c7899137b4e582954137f07",
+    "url": "/react-portfolio-1/static/media/background.cc26a3e1.jpg"
+  },
+  {
+    "revision": "e12df8baf666dfb4b1786a1a673d40ae",
+    "url": "/react-portfolio-1/static/media/class cas1.e12df8ba.jpeg"
+  },
+  {
+    "revision": "56bbdaf52927e43355f65110c9bf2993",
+    "url": "/react-portfolio-1/static/media/me.56bbdaf5.jpg"
+  },
+  {
+    "revision": "120e11335acc3289e66a935f91c2db7e",
+    "url": "/react-portfolio-1/static/media/planet.120e1133.svg"
+  },
+  {
+    "revision": "6d372dde94e7b94ccfc96579087213f4",
+    "url": "/react-portfolio-1/static/media/planet-2.6d372dde.svg"
+  },
+  {
+    "revision": "a14c984dd9654505c61446eb761954d0",
+    "url": "/react-portfolio-1/static/media/planet-3.a14c984d.svg"
+  },
+  {
+    "revision": "4bc19d953f9ae8fb763329fdfdf71dba",
+    "url": "/react-portfolio-1/static/media/planet-4.4bc19d95.svg"
+  },
+  {
+    "revision": "3bb2b6b3b12974fc41f722e6aee34742",
+    "url": "/react-portfolio-1/static/media/about.3bb2b6b3.jpg"
+  },
+  {
+    "revision": "70b38a42c0e6cbc2128aae6364ced135",
+    "url": "/react-portfolio-1/static/media/night-sky-gif-1.70b38a42.gif"
+  },
+  {
+    "revision": "32aa3e0e6890622566b73ecbcb24212d",
+    "url": "/react-portfolio-1/index.html"
+  }
+];
